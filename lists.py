2017d0b7bce1c135ab408608a1b2aa2b4b3de57b
Brandon Nguyen <lrbn86@gmail.com>
2018-07-26 17:22:44 -0400

    learned about python for loop

---
+++ lists.py
@@ -0,0 +1,26 @@
+cars = ['toyota', 'chrysler', 'nissan', 'GMC']
+
+print(cars[0])
+print(cars[1])
+print(cars[2])
+print(cars[3])
+
+# negative indices go to the last element and backwards
+print(cars[-1])
+print(cars[-2])
+
+print (cars)
+cars.append('randombrand')
+print(cars)
+cars.insert(0, 'randombo')
+print(cars)
+cars.insert(6, 'ron')
+print(cars)
+
+listo = []
+listo.insert(3, '3')
+listo.insert(4, '4')
+listo.insert(1, '1')
+listo.insert(0, '0')
+listo.insert(2, '2')
+print(listo)
